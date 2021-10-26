3de24e85619b4dbd61e0d600dd6c8816516735a6
KrzysztofKmiecik <39163249+KrzysztofKmiecik@users.noreply.github.com>
2021-10-26 12:38:12 +0200

    mail, refactor sec

--- src/main/java/pl/kmiecik/holistek/HolistekApplication.java
+++ src/main/java/pl/kmiecik/holistek/HolistekApplication.java
@@ -1,7 +1,10 @@
 package pl.kmiecik.holistek;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class HolistekApplication {
@@ -10,4 +13,15 @@ public class HolistekApplication {
         SpringApplication.run(HolistekApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void getProp(){
+        System.out.println("hasło "+propPass);
+        System.out.println("login "+propLogin);
+    }
+
+    @Value("${proxyUser}")
+    private String propLogin;
+
+    @Value("${proxyPassword}")
+    private String propPass;
 }
